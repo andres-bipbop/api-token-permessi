345aaf18babe2e7a87427097711d0cb34f8d2dfe
Andres Pellegrinelli <pellegrinelli.andres.studente@itispaleocapa.it>
2026-02-06 08:09:40 +0100

    Refactor token generation methods to swap parameters

--- api/TokenEndpoints.php
+++ api/TokenEndpoints.php
@@ -8,22 +8,22 @@
         private const ACCESS_TOKEN_KEY = "r7e9];gI#bQQ!(co<*?:89{EmGP?0R(r";
         private const REFRESH_TOKEN_KEY = "W71uX5&Hw}m|*^9wtobTRcU;)!4[OaR{";
 
-        public static function generateRefreshToken($id, $username) {
-            if (self::checkCredentials($id, $username)) {
+        public static function generateRefreshToken($username, $id) {
+            if (!self::checkCredentials($username, $id)) {
                 http_response_code(401);
-                echo json_encode(["error" => "Bad request", "message" => "Missing id or username."]);
+                echo json_encode(["http_code" => "401", "error" => "Bad request", "message" => "Missing id or username."]);
                 exit;
             }
 
             $refreshPayload = array(
-                "iss" => "https://pellegrinelliandres5ie.altervista.org/",
-                "aud" => "https://pellegrinelliandres5ie.altervista.org/website/api", //firebase jwt non lo controlla ma va messo per completteza
+                "iss" => "https://pellegrinelliandres5ie.altervista.org",
+                "aud" => "https://pellegrinelliandres5ie.altervista.org", //firebase jwt non lo controlla ma va messo per completteza
                 "iat" => time(),
                 "nbf" => time(),
                 "exp" => time() + (60 * 60 * 24 * 7),
                 "userdata" => [
-                    "id" => $id,
-                    "username" => $username
+                    "username" => $username,
+                    "id" => $id
                 ]
             );
             try {
@@ -38,32 +38,32 @@
             }
         }
 
-        public static function generateAccessToken($id, $username) {
-            if (self::checkCredentials($id, $username)) {
+        public static function generateAccessToken($username, $id) {
+            if (!self::checkCredentials($username, $id)) {
                 http_response_code(401);
-                echo json_encode(["error" => "Bad request", "message" => "Missing id or username."]);
+                echo json_encode(["http_code" => "401", "error" => "Bad request", "message" => "Missing username."]);
                 exit;
             }
             $accessPayload = array(
                 "iss" => "https://pellegrinelliandres5ie.altervista.org/",
-                "aud" => "https://pellegrinelliandres5ie.altervista.org/website/api", //firebase jwt non lo controlla ma va messo per completteza
+                "aud" => "https://pellegrinelliandres5ie.altervista.org", //firebase jwt non lo controlla ma va messo per completteza
                 "iat" => time(),
                 "nbf" => time(),
                 "exp" => time() + (60 * 10),
                 "userdata" => [
-                    "id" => $id,
-                    "username" => $username
+                    "username" => $username,
+                    "id" => $id
                 ]
             );
             try {
                 $accessJWT = JWT::encode($accessPayload, self::ACCESS_TOKEN_KEY,'HS256');
                 http_response_code(200);
-                echo json_encode(["message" => "Token generated.", "accessToken" => $accessJWT]);
+                echo json_encode(["http_code" => "200", "message" => "Token generated.", "accessToken" => $accessJWT]);
             }
             catch (UnexpectedValueException $e) 
             {
             	http_response_code(500);
-                echo json_encode(["error" => $e->getMessage(), "message" => "Internal Server Error."]);
+                echo json_encode(["http_code" => "500", "error" => $e->getMessage(), "message" => "Internal Server Error."]);
             }
         }
 
@@ -71,10 +71,10 @@
             try {
                 $decoded = JWT::decode($accessToken, new Key(self::ACCESS_TOKEN_KEY, "HS256"));
                 http_response_code(200);
-                echo json_encode(["message" => "Access Granted.", "data" => $decoded]);
+                echo json_encode(["http_code" => "200", "message" => "Access Granted.", "data" => $decoded]);
             } catch (Exception $e) {
                 http_response_code(401);
-                echo json_encode(["error" => $e->getMessage(), "message" => "Access Denied, invalid Token."]);
+                echo json_encode(["http_code" => "401", "error" => $e->getMessage(), "message" => "Access Denied, invalid Token."]);
             }
         }
 
@@ -83,14 +83,14 @@
                 $decoded = JWT::decode($refreshToken, new Key(self::REFRESH_TOKEN_KEY, "HS256"));
                 $username = $decoded->userdata->username;
                 $id = $decoded->userdata->id;
-                self::generateAccessToken($id, $username);
+                self::generateAccessToken($username, $id);
             } catch (Exception $e) {
                 http_response_code(401);
-                echo json_encode(["error" => $e->getMessage(), "message" => "Access Denied, invalid Token."]);
+                echo json_encode(["http_code" => "401", "error" => $e->getMessage(), "message" => "Access Denied, invalid Token."]);
             }
         }
-        private static function checkCredentials($id, $username) {
-            if ($id === null || $username === null) {
+        private static function checkCredentials($username, $id) {
+            if ($username === null || $id === null) {
                 return false;
             } else return true;
         }
